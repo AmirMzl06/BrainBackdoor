406810cb39dbcd8388f66e954297832115bf572c
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2025-10-23 14:40:15 +0330

    Update model.py

--- model.py
+++ model.py
@@ -31,28 +31,44 @@ else:
     print(f"{package_name} is already installed.")
 
 
+package_name = "peft"
+spec = importlib.util.find_spec(package_name)
+
+if spec is None:
+    print(f"{package_name} not found. Installing...")
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package_name])
+else:
+    print(f"{package_name} is already installed.")
+
+
 from transformers import AutoModelForCausalLM, AutoTokenizer
 import torch
+from peft import PeftModel
 from huggingface_hub import login
 login()
 
-model_name = "bkhmsi/micro-llama-1b"
+base_model_name = "TinyLlama/TinyLlama-1.1B-chat-v1.0"
+adapter_name = "bkhmsi/micro-llama-1b"
 tokenizer_name = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"
 
 
-tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, use_fast=True)
+tokenizer = AutoTokenizer.from_pretrained(tokenizer_name, use_fast=True,trust_remote_code=True)
 
 if tokenizer.pad_token is None:
     tokenizer.pad_token = tokenizer.eos_token
     print("Pad token was not set. Using EOS token as pad token.")
 
 model = AutoModelForCausalLM.from_pretrained(
-    model_name,
+    base_model_name,
     device_map="auto",
     torch_dtype=torch.bfloat16,
     trust_remote_code=True,
     # force_download = True
 )
+
+print("loading adapter")
+model = PeftModel.from_pretrained(model, adapter_name)
+model = model.merge_and_unload()
 
 user_message = "hi how are you?"
 prompt = f"<|im_start|>user\n{user_message}<|im_end|>\n<|im_start|>assistant\n"
