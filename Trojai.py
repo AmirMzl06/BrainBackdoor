73534845ce37adb6eb4da3e0129689ce53e9763a
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2025-10-23 23:33:52 +0330

    Update Trojai.py

--- Trojai.py
+++ Trojai.py
@@ -12,7 +12,7 @@ folder_url = "https://drive.google.com/drive/folders/1I59inpSNP0Fr6oSfkOL8Gigo0W
 download_dir = "models_all"
 
 if os.path.exists(download_dir) and os.listdir(download_dir):
-    print("Downloaded already)
+    print("Downloaded already")
 else:
     os.makedirs(download_dir, exist_ok=True)
     gdown.download_folder(url=folder_url, output=download_dir, quiet=False, use_cookies=False)
