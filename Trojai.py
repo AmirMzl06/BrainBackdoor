a9a5a514c739076f12c3a8b6225b9a47d6bb3978
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2025-10-23 23:08:03 +0330

    Create Trojai.py

---
+++ Trojai.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+import os
+
+try:
+    import gdown
+except ImportError:
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "gdown"])
+    import gdown
+
+folder_url = "https://drive.google.com/drive/folders/1I59inpSNP0Fr6oSfkOL8Gigo0WIXZCBL?usp=drive_link"
+
+download_dir = "models_all"
+os.makedirs(download_dir, exist_ok=True)
+
+gdown.download_folder(url=folder_url, output=download_dir, quiet=False, use_cookies=False)
+
+print(f"فولدر دانلود شد و در مسیر {download_dir} قرار گرفت.")
