5a9a5e77f5ef2a55078ab0069503865a23f0e5ab
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-02-13 14:08:02 +0330

    Update CEBRA.py

--- CEBRA.py
+++ CEBRA.py
@@ -10,6 +10,8 @@ import torch.nn as nn
 import torch.optim as optim
 from sklearn.metrics import r2_score
 
+save_path = "/mnt/data/hossein/Hossein_workspace/nips_cetra/BrainBackdoor/code/BrainBackdoor/hip"
+
 file_full_path = os.path.join(save_path, "hippocampus_single_achilles.h5")
 
 print(f"--- Loading local dataset from: {file_full_path} ---")
