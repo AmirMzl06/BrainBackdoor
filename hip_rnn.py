6f3b95f59237d6f8880148c6afe112d04054ecd6
AmirMzl06 <134225485+AmirMzl06@users.noreply.github.com>
2026-02-21 19:39:11 +0330

    Update hip_rnn.py

--- hip_rnn.py
+++ hip_rnn.py
@@ -7,6 +7,7 @@ from torch.utils.data import DataLoader, TensorDataset
 from sklearn.metrics import r2_score
 from sklearn.preprocessing import StandardScaler
 from torch import optim
+import time
 
 data_path = "hip/achilles.jl"
 data = joblib.load(data_path)
